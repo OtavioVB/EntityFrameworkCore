c83beeb74b07b3edc0d250ebfc5b55d4e24bef8f
OtavioVB <otaviovb.developer@gmail.com>
2022-08-28 10:56:09 -0300

    feat(dbcontext): `database connection`

--- EFCore.Infra/DataContext/DataContext.cs
+++ EFCore.Infra/DataContext/DataContext.cs
@@ -5,4 +5,8 @@ namespace EFCore.Infra.DataContext;
 public class DataContext : DbContext
 {
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMySql("server=127.0.0.1;database=efcore;uid=otavio;pwd=1234;port=3306", ServerVersion.AutoDetect(new MySqlConnector.MySqlConnection("server=127.0.0.1;database=efcore_migrations;uid=otavio;pwd=1234;port=3306")));
+    }
 }
